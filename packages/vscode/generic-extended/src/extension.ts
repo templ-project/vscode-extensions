ee9cc741912c3e5b6097630d55c54e9b47aa34f3
Dragos Cirjan <dragos.cirjan@gmail.com>
2025-07-23 01:10:53 +0300

    fix: preparing release 3

--- packages/vscode/generic-extended/src/extension.ts
+++ packages/vscode/generic-extended/src/extension.ts
@@ -5,7 +5,7 @@ import * as vscode from 'vscode';
  * Your extension is activated the very first time the command is executed
  */
 export function activate(context: vscode.ExtensionContext) {
-    console.log('[templ-project-1753222089791] Generic Extended Extension Pack extension is now active!');
+    console.log('[templ-project-1753222226491] Generic Extended Extension Pack extension is now active!');
 
     // Apply settings when extension activates
     applySettings();
@@ -13,12 +13,12 @@ export function activate(context: vscode.ExtensionContext) {
     // Register commands
     const applySettingsCommand = vscode.commands.registerCommand('templ-project.generic-extended-vscode.applySettings', () => {
         applySettings();
-        vscode.window.showInformationMessage('[templ-project-1753222089791] Generic Extended Extension Pack settings applied!');
+        vscode.window.showInformationMessage('[templ-project-1753222226491] Generic Extended Extension Pack settings applied!');
     });
 
     const resetSettingsCommand = vscode.commands.registerCommand('templ-project.generic-extended-vscode.resetSettings', () => {
         resetSettings();
-        vscode.window.showInformationMessage('[templ-project-1753222089791] Generic Extended Extension Pack settings reset!');
+        vscode.window.showInformationMessage('[templ-project-1753222226491] Generic Extended Extension Pack settings reset!');
     });
 
     context.subscriptions.push(applySettingsCommand);
@@ -220,5 +220,5 @@ function resetSettings() {
  * This method is called when your extension is deactivated
  */
 export function deactivate() {
-    console.log('[templ-project-1753222089791] Generic Extended Extension Pack extension is now deactivated!');
+    console.log('[templ-project-1753222226491] Generic Extended Extension Pack extension is now deactivated!');
 }
