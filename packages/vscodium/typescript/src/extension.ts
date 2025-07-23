fd8adfcd141f4cfbcdb0d8c77170d5df93241d88
Dragos Cirjan <dragos.cirjan@gmail.com>
2025-07-24 00:59:19 +0300

    fix: preparing release 9

--- packages/vscodium/typescript/src/extension.ts
+++ packages/vscodium/typescript/src/extension.ts
@@ -5,7 +5,7 @@ import * as vscode from 'vscode';
  * Your extension is activated the very first time the command is executed
  */
 export function activate(context: vscode.ExtensionContext) {
-    console.log('[templ-project-1753307540332] Typescript Extension Pack extension is now active!');
+    console.log('[templ-project-1753307920963] Typescript Extension Pack extension is now active!');
 
     // Apply settings when extension activates
     applySettings();
@@ -13,12 +13,12 @@ export function activate(context: vscode.ExtensionContext) {
     // Register commands
     const applySettingsCommand = vscode.commands.registerCommand('templ-project.typescript-vscodium.applySettings', () => {
         applySettings();
-        vscode.window.showInformationMessage('[templ-project-1753307540332] Typescript Extension Pack settings applied!');
+        vscode.window.showInformationMessage('[templ-project-1753307920963] Typescript Extension Pack settings applied!');
     });
 
     const resetSettingsCommand = vscode.commands.registerCommand('templ-project.typescript-vscodium.resetSettings', () => {
         resetSettings();
-        vscode.window.showInformationMessage('[templ-project-1753307540332] Typescript Extension Pack settings reset!');
+        vscode.window.showInformationMessage('[templ-project-1753307920963] Typescript Extension Pack settings reset!');
     });
 
     context.subscriptions.push(applySettingsCommand);
@@ -74,5 +74,5 @@ function resetSettings() {
  * This method is called when your extension is deactivated
  */
 export function deactivate() {
-    console.log('[templ-project-1753307540332] Typescript Extension Pack extension is now deactivated!');
+    console.log('[templ-project-1753307920963] Typescript Extension Pack extension is now deactivated!');
 }
